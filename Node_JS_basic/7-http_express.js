0aadf4c3b697cc7eac9d76803fb2b98bf020fa41
Haji <hacih4451@gmail.com>
2026-05-30 12:24:38 +0400

    Update 7-http_express.js

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,51 +1,5 @@
-// recreate the small HTTP server using Express
-// HTTP server should listen on port 1245
-// It should return plain text
-// When the URL path is /, it should display Hello Holberton School!
-// When the URL path is /students, it should display This is the list of our students
-// followed by the same content as the file 3-read_file_async.js (with and without the database)
-// the name of the database must be passed as argument of the file
-// CSV file can contain empty lines (at the end) - and they are not a valid student!
-
-const express = require('express');
-const countStudents = require('./3-read_file_async.js');
-
-const app = express();
-const port = 1245;
-
-app.get('/', (_req, res) => {
-  res.send('Hello Holberton School!');
-});
-
-app.get('/students', async (_req, res) => {
-  res.write('This is the list of our students\n');
-  await countStudents(process.argv[2])
-    .then((data) => {
-      const fields = Object.keys(data);
-      const total = fields.reduce(
-        (acc, curr) => acc + data[curr].numStudents,
-        0,
-      );
-      res.write(`Number of students: ${total}\n`);
-      for (let i = 0; i < fields.length; i += 1) {
-        res.write(
-          `Number of students in ${fields[i]}: ${
-            data[fields[i]].numStudents
-          }. `,
-        );
-        res.write(`List: ${data[fields[i]].names.join(', ')}`);
-        if (i < fields.length - 1) {
-          res.write('\n');
-        }
-      }
-    })
-    .catch((err) => {
-      res.write(err.message);
-    })
-    .finally(() => {
-      res.end();
-    });
-});
-app.listen(port);
-
-module.exports = app;
+const express = require('express'); const fs = require('fs'); const app = express();
+function getStudentsReport(path) { return fs.promises.readFile(path, 'utf8').then((data) => { const rows = data.split('\n').filter((line) => line.trim() !== ''); const students = rows.slice(1); const byField = {}; students.forEach((student) => { const [firstname, , , field] = student.split(','); if (!byField[field]) { byField[field] = []; } byField[field].push(firstname); }); const lines = [`Number of students: ${students.length}`]; Object.keys(byField).forEach((field) => { lines.push(`Number of students in ${field}: ${byField[field].length}. List: ${byField[field].join(', ')}`); }); return lines.join('\n'); }).catch(() => { throw new Error('Cannot load the database'); }); }
+app.get('/', (req, res) => { res.send('Hello Holberton School!'); });
+app.get('/students', (req, res) => { const dbPath = process.argv[2]; getStudentsReport(dbPath).then((report) => { res.send(`This is the list of our students\n${report}`); }).catch((error) => { res.send(`This is the list of our students\n${error.message}`); }); });
+app.listen(1245); module.exports = app;
