0eb1aec741009fd99782a8b95ab798c80ff15146
Haji <hacih4451@gmail.com>
2026-05-30 12:03:15 +0400

    Refactor HTTP server code for clarity and consistency

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,18 +1,18 @@
+// Create a small HTTP server using Node's HTTP module
+
+// In a file named 4-http.js, create a small HTTP server using the http module:
+// It should be assigned to the variable app and this one must be exported
+// HTTP server should listen on port 1245
+// Displays Hello Holberton School! in the page body for any endpoint as plain text
+
 const http = require('http');
 
-const PORT = 1245;
+const port = 1245;
 
-// Create the server instance
-const app = http.createServer((req, res) => {
-  // Set the response status and content type header to plain text
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  
-  // Send the body response and close the connection
+const app = http.createServer((_req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
-});
+}).listen(port);
 
-// Make the server listen on port 1245
-app.listen(PORT);
-
-// Export the server instance
 module.exports = app;
